8bb863bed83c5e9b623db7575f769a4cae3c3ddb
Girum-Haile <girum97gebriel@gmail.com>
2019-12-03 17:27:51 +0300

    type

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,10 @@
 print("hello world")
 # number values doesn't require quotes
 print(12)
+# type() is used to identify the type of the value
+# this program prints out "str" which is string
+print(type("hello world"))
+# this program prints out "int" which is integer
+print(type(12))
+
+
