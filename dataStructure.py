da247bde03c7d4db91254f16083dc136078d230b
Girum-Haile <girum97gebriel@gmail.com>
2019-12-06 11:10:37 +0300

    data structure

---
+++ dataStructure.py
@@ -0,0 +1,14 @@
+# List- is a collection which is ordered, allow duplicated members and changeable
+# list creation
+list1 = [2, 3, 4, 5]
+list2 = ['a', 'b', 'c']
+# mixed type of values
+list3 = ['a', 'b', 2]
+# allow duplicated values
+list4 = [2, 3, 4, 4, 2, 6, 6, 7, 8]
+
+# printing list
+print(list1[0])  # we use indexing
+print(list1)
+# to add values to a list we use append() method
+
