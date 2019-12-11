e898e14552ac2b8baac42b2ad6b904149ec4c185
Girum-Haile <girum97gebriel@gmail.com>
2019-12-11 14:20:48 +0300

    while loop

---
+++ while loop.py
@@ -0,0 +1,33 @@
+# while loop are used to execute an instruction until a given condition is satisfied
+count = 0
+while count < 3:
+    count = count + 1
+    print("Hello")
+
+# using else statement with while loop
+# count = 0
+# while count < 3:
+#     count = count + 1
+#     print("Hello world")
+# else:
+#     print("world")
+#
+# # break statement -it brings control out of the loop
+# i = 1
+# while i < 100:
+#     print(i)
+#     if i == 20:
+#         break
+#     i = i+1
+
+# continue-  It returns the control to the beginning of the loop
+i = 1
+while i < 100:
+    i = i + 1
+    if i < 50:
+        continue
+    print(i)
+
+
+
+
