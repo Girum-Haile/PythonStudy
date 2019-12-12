ec27f889f8df6553d7c51d2fdf0124046eb5df57
Girum-Haile <girum97gebriel@gmail.com>
2019-12-12 13:34:49 +0300

    'class'

--- OOP-Class.py
+++ OOP-Class.py
@@ -7,9 +7,8 @@ class New:
     pass
 
 
-
 class Dogs:
-    type = "Doberman" # class attributes
+    type = "Doberman"  # class attributes
     atr = "mamal"
 
     def method(self):
@@ -30,10 +29,34 @@ class Person:
         print(f'Hello {self.name} you are {self.age} years old'.format(self.name, self.age))
 
 
-person1 = Person("girum",12)
+person1 = Person("girum", 12)
 person1.name_age()
 
+# instance variable- are for data each for unique instance.
+# class instances are for attributes and methods shared by all instance of the class
 
 
+class Family:
+    family = 'alex'  # class variable
+
+    def __init__(self, name, age):
+        self.name = name  # instance variable
+        self.age = age
+
+    def setcolor(self, color):
+        self.color = color
+
+    def getcolor(self):
+        return self.color
 
 
+namee = Family("alex",21)
+namee.setcolor("blue")
+print(namee.getcolor())
+
+
+def main():
+    print('hello world')
+
+
+if __name__ == '__main__': main()
