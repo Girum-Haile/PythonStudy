81a135810e26f2524cb4d0d2262beee8e6ac78e0
Girum-Haile <girum97gebriel@gmail.com>
2019-12-16 22:53:58 +0300

    multithreading

---
+++ OOP-Multithreading.py
@@ -0,0 +1,111 @@
+# Multithreading-is defined as the ability of a processor to execute multiple threads concurrently.
+# importing the threading module
+import threading
+import os
+
+
+def print_cube(num):
+    """
+    function to print cube of given num
+    """
+    print("Cube: {}".format(num * num * num))
+
+
+def print_square(num):
+    """
+    function to print square of given num
+    """
+    print("Square: {}".format(num * num))
+
+
+if __name__ == "__main__":
+    # creating thread
+    t1 = threading.Thread(target=print_square, args=(10,))
+    t2 = threading.Thread(target=print_cube, args=(10,))
+
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+
+    # wait until thread 1 is completely executed
+    t1.join()
+    # wait until thread 2 is completely executed
+    t2.join()
+
+    # both threads completely executed
+    print("Done!")
+
+# prints thread name and corresponding process
+
+
+def task1():
+    print("Task 1 assigned to thread: {}".format(threading.current_thread().name))
+    print("ID of process running task 1: {}".format(os.getpid()))
+
+
+def task2():
+    print("Task 2 assigned to thread: {}".format(threading.current_thread().name))
+    print("ID of process running task 2: {}".format(os.getpid()))
+
+
+if __name__ == "__main__":
+    # print ID of current process
+    print("ID of process running main program: {}".format(os.getpid()))
+
+    # print name of main thread
+    print("Main thread name: {}".format(threading.main_thread().name))
+
+    # creating threads
+    t1 = threading.Thread(target=task1, name='t1')
+    t2 = threading.Thread(target=task2, name='t2')
+
+    # starting threads
+    t1.start()
+    t2.start()
+
+    # wait until all threads finish
+    t1.join()
+    t2.join()
+
+
+# race condition
+# global variable x
+x = 0
+
+
+def increment():
+
+    # function to increment global variable x
+    global x
+    x += 1
+
+
+def thread_task():
+    # task for thread calls increment function 100000 times.
+    for _ in range(100000):
+        increment()
+
+
+def main_task():
+    global x
+    # setting global variable x as 0
+    x = 0
+
+    # creating threads
+    t1 = threading.Thread(target=thread_task)
+    t2 = threading.Thread(target=thread_task)
+
+    # start threads
+    t1.start()
+    t2.start()
+
+    # wait until threads finish their job
+    t1.join()
+    t2.join()
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        main_task()
+        print("Iteration {0}: x = {1}".format(i, x))
